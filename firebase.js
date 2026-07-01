d5c8b1aff9c872eea14007cb8abb3cb364927b33
POKEMON <sid.subramani14@gmail.com>
2026-07-01 08:17:14 -0500

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+// Firebase configuration (YOU replace this later)
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
